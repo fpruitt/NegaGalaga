0ea077f90fc5cb480e99244e988b5dd53379768b
jshiraef <joeshiraef@gmail.com>
2014-11-08 20:06:27 -0500

    actually saved the code this time

--- NegaSpaceInvaders/Assets/ShootingStuff.cs
+++ NegaSpaceInvaders/Assets/ShootingStuff.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class ShootingStuff : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	public Transform bullet;
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if(Input.GetKeyDown ("w") || Input.GetKeyDown ("Space"))   {
+			Instantiate(bullet, new Vector3(transform.position), Quaternion.identity) as Transform;
+		}
 	}
 }
